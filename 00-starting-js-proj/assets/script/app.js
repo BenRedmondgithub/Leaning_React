b53ddbdea78e8d5fd3cdacba7fbf0941d5aae7ee
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-04-14 11:06:14 +0100

    added files

---
+++ 00-starting-js-proj/assets/script/app.js
@@ -0,0 +1,29 @@
+let userMessage = "Hello World";
+
+console.log (userMessage);
+console.log (userMessage.length);
+console.log (userMessage.toUpperCase());
+console.log (userMessage.toLowerCase());
+
+console.log (10 + 5);
+console.log (10 - 5);
+console.log (10 * 5);
+console.log (10 / 5);
+
+
+console.log (10 === 10);
+
+console.log (10 === 5);
+
+if (10 === 10) {
+    console.log ("10 is equal to 10");
+}
+else {
+    console.log ("10 is not equal to 10");
+}
+
+function GreetUser () {
+    console.log ("Hello! How are you?");
+}
+
+GreetUser ();
